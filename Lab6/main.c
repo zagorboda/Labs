65ce6ef298a3f3b49d85ce925099bf140f748451
zagorboda <zagorboda@gmail.com>
2019-11-05 22:02:24 +0300

    The method of simple iteration isn't working

--- Lab6/main.c
+++ Lab6/main.c
@@ -4,7 +4,9 @@
 int main(){
     int n,check,length,i,j;
     char n_1[100];
-    double **a,**x,**b,**xp;
+    double **a;
+    double *x,*b,*xp,*a_1;
+    double a_s=0;
 
     while(1){
         check = 0;
@@ -32,9 +34,10 @@ int main(){
     for ( i = 0; i < n; i++)
         a[i] = (double *) calloc (n, sizeof (double));
 
-    b = (double **) calloc (n, sizeof (double *));
-    for ( i = 0; i < n; i++)
-        b[i] = (double *) calloc (n, sizeof (double));
+    b = (double *) calloc (n, sizeof (double));
+    xp = (double *) calloc (n, sizeof (double));
+    x = (double *) calloc (n, sizeof (double));
+    a_1 = (double *) calloc (n, sizeof (double));
 
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
@@ -45,53 +48,55 @@ int main(){
 
     for(i=0;i<n;i++){
         printf("Enter B[%d] element: ", i);
-        scanf("%lf", &b[i][0]);
+        scanf("%lf", &b[i]);
     }
 
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
-            printf("%lf ", a[i][j]);
+            printf("%lf", a[i][j]);
         }
         printf(" = ");
-        printf("%lf ", b[i][0]);
+        printf("%lf ", b[i]);
         printf("\n");
     }
 
+    xp[0] = b[0] / a[0][0];
+
     /*for(i=0;i<n;i++){
-        printf("%lf", b[i][0]);
+        xp[i] = b[i] / a[i][i];
     }*/
 
-    /*int i, j, n, a, b;
-
-    int ( * ( * p ) [] ) [];
-
-    printf ( "\n\tEnter the size of the matrix in the form aXb\t\n" );
-
-    scanf ( "%d", &a);
-
-    p = malloc ( a * sizeof ( int ( * ) [] ) );
-
-    for ( i = 0;i < a;i++ ) {
-        (*p)[i] = malloc (a * sizeof(int));
-        printf ( "\t\bEnter Column %d\t\n", i );
-        for ( j = 0;j < a;j++ )
-            scanf ( "%d", & ( * ( *p ) [i] ) [j] );
+    /*for(i=0;i<n;i++){
+        a_s = 0;
+        for(j=0;j<n;j++){
+            if(j != i){
+                a_s += a[i][j];
+            }
+        a_1[i] = a_s;
+        }
     }*/
 
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(j != i){
+                x[i] = b[i] / a[i][i] - (a[i][j] * xp[j]) / a[i][i];
+                xp[j+1] = x[i];
+                /*x[i] = (b[i] - (a_1[j] * xp[j])) /a[i][j];
+                xp[j+1] = x[i];*/
+            }
+        }
+    }
+    printf("\n");
+    for(i=0;i<n;i++){
+        printf("%lf \n", x[i]);
+    }
 
 
 
-    /*int n;
-    scanf("%d", &n);
-    int **sArray;
-    sArray = (int **)malloc(n * sizeof(int *));
-
-    int i,j;
-    for(i = 0; i < n; i++)
-    {
-        sArray[i] = (int *)malloc(2 * sizeof(int));
-        scanf("%d %d", &sArray[i][1], &sArray[i][2]);
-    }*/
+    for ( i = 0; i < n; i++)
+        free (a[i]);
+    free (a);
+    free (b);
 
     return 0;
 }
