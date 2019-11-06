195625d2f411a7649f56f57eeb1516e17743908b
zagorboda <zagorboda@gmail.com>
2019-11-06 21:18:12 +0300

    Bad ittaretive formula

--- Lab6/main.c
+++ Lab6/main.c
@@ -2,11 +2,27 @@
 #include <stdlib.h>
 
 int main(){
-    int n,check,length,i,j;
+    int n,check,length,i,j,check_4;
     char n_1[100];
     double **a;
     double *x,*b,*xp,*a_1;
-    double a_s=0;
+    double a_s=0, delta=0, e, check_e, k;
+    char e_1[100];
+
+    /*double *xk, *xk1;
+    double t, l1, l2, max, Axk = 0, eps;
+    int k, count = 0;
+    int compare(){
+        max = abs(xk[0] - xk1[0]);
+        for (i = 1; i < n; i++){
+            if (abs(xk[i] - xk1[i]) > max)
+            max = abs(xk[i] - xk1[i]);
+        }
+        if (max <= eps)
+            return 1;
+        else
+            return 0;
+    }*/
 
     while(1){
         check = 0;
@@ -29,6 +45,77 @@ int main(){
         }
 
     }
+
+    i = 0;
+    while(1){
+        printf("Enter e:");
+        scanf("%s", &e_1);
+        e = atof(e_1);
+        length = strlen(e_1);
+        for (i = 0; i < length; i++){
+            j = e_1[i];
+            if (j == 45 || j == 46 || isdigit(e_1[i])){
+                check_e += 1;
+            }
+            else
+                continue;
+        }
+
+        if(check_e == length){
+            check_4 = 1;
+        }
+
+        printf("e = %lf \n", e);
+
+        if(check_4 == 1 && e > 0){
+            break;
+        }else{
+            system("cls");
+            printf("Try another value \n");
+        }
+    }
+
+    /*a = (double**)malloc(n*sizeof(double*));
+    for (i = 0; i < n; i++)
+        a[i] = (double*)malloc((n + 1)*sizeof(double));
+
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            printf("Enter A[%d][%d] element: ", i, j);
+            scanf("%lf", &a[i][j]);
+        }
+    }
+
+    scanf("%lf %lf", &l1, &l2);
+
+    xk = (double*)malloc(n*sizeof(double));
+    xk1 = (double*)malloc(n*sizeof(double));
+    for (i = 0; i < n; i++){
+        xk[i] = 1;
+        xk1[i] = 0;
+    }
+    t = 2 / (l1 + l2);
+
+    do{
+        for (i = 0; i < n; i++){
+            for (j = 0; j < n; j++){
+                Axk += a[i][j] * xk[j];
+            }
+        xk1[i] = xk[i] - t*(Axk - a[i][n]);
+        Axk = 0;
+        }
+        for (k = 0; k < n; k++){
+            xk[k] = xk1[k];
+        }
+        count++;
+     } while (compare() == 0);
+
+     for (i = 0; i < n; i++)
+        printf("%.4lf \n", xk1[i]);*/
+
+
+
+
 
     a = (double **) calloc (n, sizeof (double *));
     for ( i = 0; i < n; i++)
@@ -60,37 +147,36 @@ int main(){
         printf("\n");
     }
 
-    xp[0] = b[0] / a[0][0];
-
-    /*for(i=0;i<n;i++){
+    for(i=0;i<n;i++){
         xp[i] = b[i] / a[i][i];
-    }*/
+        printf("xp = %lf \n", xp[i]);
+    }
 
-    /*for(i=0;i<n;i++){
+    for(i=0;i<n;i++){
         a_s = 0;
         for(j=0;j<n;j++){
             if(j != i){
                 a_s += a[i][j];
+                printf("as = %lf \n", a_s);
             }
-        a_1[i] = a_s;
         }
-    }*/
+        a_1[i] = a_s;
+        printf("a1 = %lf \n", a_1[i]);
+    }
+
 
     for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            if(j != i){
-                x[i] = b[i] / a[i][i] - (a[i][j] * xp[j]) / a[i][i];
-                xp[j+1] = x[i];
-                /*x[i] = (b[i] - (a_1[j] * xp[j])) /a[i][j];
-                xp[j+1] = x[i];*/
-            }
-        }
+        do{
+            x[i] = (b[i] - a_1[i]*xp[i]) / a[i][i];
+            delta = abs(x[i] - xp[i]);
+            xp[i] = x[i];
+            printf("delta = %lf", delta);
+        }while(delta > e);
     }
     printf("\n");
     for(i=0;i<n;i++){
         printf("%lf \n", x[i]);
     }
-
 
 
     for ( i = 0; i < n; i++)
