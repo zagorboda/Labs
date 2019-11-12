aa1bc9330d7a2351c5e780d90a78f4a08cf66ef8
zagorboda <zagorboda@gmail.com>
2019-11-12 12:32:46 +0300

    Scanning spaces

--- Lab5/main.c
+++ Lab5/main.c
@@ -3,7 +3,7 @@
 #include<string.h>
 
 int main(){
-    int m,n=100;
+    int m,n;
     char a[100];
     char b[100];
     int length_1, num_check;
@@ -34,36 +34,61 @@ int main(){
 
     }
 
+    while(1){
+        check = 0;
+        printf("Enter n: ");
+        scanf("%s", &n_1);
+        length_1 = strlen(n_1);
+        for (i = 0; i < length_1; i++)
+            if (!isdigit(n_1[i])){
+                continue;
+            }
+            else
+                check += 1;
+
+        if(check == length_1){
+            n = atoi(n_1);
+            break;
+        }else{
+            system("cls");
+            printf("Incorrect input. Try another values. \n");
+        }
+
+    }
+
     char list[m][n];
     char *addr[m];
     char *c;
 
-    for (i=0;i<m;i++){
-        scanf("%s", list[i]);
+    for (i=0;i<=m;i++){
+        fgets (list[i], 100, stdin);
     }
 
-    for (i=0; i<m; i++)
+    for (i=1; i<=m; i++)
         addr[i] = list[i];
 
-    for (i=0;i<m;i++){
+    for (i=1;i<=m;i++){
         printf("List[%d] = %s \n", i, addr[i]);
     }
     printf("\n");
 
-    for(i = 0;i < m-1;i++){
-        for(j = i+1;j < m;j++){
+    for(i = 1;i <= m-1;i++){
+        for(j = i+1;j <= m;j++){
             if(strcmp(addr[j-1], addr[j]) > 0){
                 c = addr[j-1];
-                addr[j-1] = addr[j];
+                addr[j-1] = addr[j];S
                 addr[j] = c;
             }
         }
     }
     printf("\n");
 
-    for (i=0;i<m;i++){
+    for (i=1;i<=m;i++){
         printf("List[%d] = %s \n", i, addr[i]);
     }
+
+    free (addr);
+    free (c);
 
     return 0;
 }
