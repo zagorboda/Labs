e1586a03874458ad116c17267731ecb5f2aed83a
zagorboda <zagorboda@gmail.com>
2019-11-13 00:09:22 +0300

    Refactor code

--- Lab6/main.c
+++ Lab6/main.c
@@ -2,27 +2,12 @@
 #include <stdlib.h>
 
 int main(){
-    int n,check,length,i,j,check_4, counter, stop, k, check_sum_1;
+    int n, check, length, i, j, check_4, k, check_sum_1;
     char n_1[100];
     double **a;
-    double *x,*b,*xp,*a_1;
-    double a_s=0, delta, e, check_e, a0, check_sum=0;
+    double *x,*b,*xp;
+    double delta, e, check_e, check_sum;
     char e_1[100];
-
-    /*double *xk, *xk1;
-    double t, l1, l2, max, Axk = 0, eps;
-    int k, count = 0;
-    int compare(){
-        max = abs(xk[0] - xk1[0]);
-        for (i = 1; i < n; i++){
-            if (abs(xk[i] - xk1[i]) > max)
-            max = abs(xk[i] - xk1[i]);
-        }
-        if (max <= eps)
-            return 1;
-        else
-            return 0;
-    }*/
 
     while(1){
         check = 0;
@@ -46,7 +31,6 @@ int main(){
 
     }
 
-    i = 0;
     while(1){
         printf("Enter e:");
         scanf("%s", &e_1);
@@ -73,48 +57,6 @@ int main(){
         }
     }
 
-    /*a = (double**)malloc(n*sizeof(double*));
-    for (i = 0; i < n; i++)
-        a[i] = (double*)malloc((n + 1)*sizeof(double));
-
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            printf("Enter A[%d][%d] element: ", i, j);
-            scanf("%lf", &a[i][j]);
-        }
-    }
-
-    scanf("%lf %lf", &l1, &l2);
-
-    xk = (double*)malloc(n*sizeof(double));
-    xk1 = (double*)malloc(n*sizeof(double));
-    for (i = 0; i < n; i++){
-        xk[i] = 1;
-        xk1[i] = 0;
-    }
-    t = 2 / (l1 + l2);
-
-    do{
-        for (i = 0; i < n; i++){
-            for (j = 0; j < n; j++){
-                Axk += a[i][j] * xk[j];
-            }
-        xk1[i] = xk[i] - t*(Axk - a[i][n]);
-        Axk = 0;
-        }
-        for (k = 0; k < n; k++){
-            xk[k] = xk1[k];
-        }
-        count++;
-     } while (compare() == 0);
-
-     for (i = 0; i < n; i++)
-        printf("%.4lf \n", xk1[i]);*/
-
-
-
-
-
     a = (double **) calloc (n, sizeof (double *));
     for ( i = 0; i < n; i++)
         a[i] = (double *) calloc (n, sizeof (double));
@@ -122,7 +64,6 @@ int main(){
     b = (double *) calloc (n, sizeof (double));
     xp = (double *) calloc (n, sizeof (double));
     x = (double *) calloc (n, sizeof (double));
-    a_1 = (double *) calloc (n, sizeof (double));
 
     while(1){
         for(i=0;i<n;i++){
@@ -151,7 +92,6 @@ int main(){
             }
         }
 
-
         check_sum_1 = 0;
         for(i=0;i<n;i++){
             check_sum = 0;
@@ -170,11 +110,6 @@ int main(){
         }
 
     }
-
-    /*for(i=0;i<n;i++){
-        printf("Enter B[%d] element: ", i);
-        scanf("%lf", &b[i]);
-    }*/
 
     for(i=0;i<n;i++){
         while(1){
@@ -200,7 +135,6 @@ int main(){
         }
     }
 
-
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
             printf("%lf", a[i][j]);
@@ -209,61 +143,6 @@ int main(){
         printf("%lf ", b[i]);
         printf("\n");
     }
-
-    /*for(i=0;i<n;i++){
-        xp[i] = b[i] / a[i][i];
-        printf("xp = %lf \n", xp[i]);
-    }
-
-    for(i=0;i<n;i++){
-        a_s = 0;
-        for(j=0;j<n;j++){
-            if(j != i){
-                a_s += a[i][j];
-                printf("as = %lf \n", a_s);
-            }
-        }
-        a_1[i] = a_s;
-        printf("a1 = %lf \n", a_1[i]);
-    }
-
-
-
-    for(i=0;i<n;i++){
-            do{
-                x[i] = (b[i] - a_1[i]*xp[i]) / a[i][i];
-                delta = fabs(x[i] - xp[i]);
-                xp[i] = x[i];
-                printf("i = \n %d", i);
-                printf("x = %lf \n", x);
-                printf("delta = %lf \n", delta);
-            }while(delta > e);
-    }printf("\n");
-    for(i=0;i<n;i++){
-        printf("%lf \n", x[i]);
-    }*/
-
-
-
-    /*for(i=0;i<n;i++){
-        a_s = 0;
-        for(j=0;j<n;j++){
-            if(j != i){
-                a_s += - 1 * a[i][j] / a[i][i];
-                //printf("as = %lf \n", a_s);
-            }
-        }
-        a_1[i] = a_s;
-        //printf("a1 = %lf \n", a_1[i]);
-    }
-
-    for(i=0;i<n;i++){
-        do{
-            x[i] = b[i] / a[i][i]  + a_1[i]*xp[i];
-            delta = fabs(xp[i] - x[i]);
-            xp[i] = x[i];
-        }while(delta > e);
-    }*/
 
     for(i=0;i<n;i++){
         xp[i] = b[i] / a[i][i];
@@ -288,58 +167,6 @@ int main(){
     for(i=0;i<n;i++){
         printf("%lf \n", x[i]);
     }
-
-        //counter++;
-
-    /*for(i = 0; i < n; i++){
-        xp[i] = b[i] / a[i][i];
-    }
-
-    for(i=0;i<n;i++){
-        a_s = 0;
-        for(j=0;j<n;j++){
-            if(j != i){
-                a_s += a[i][j];
-                //printf("as = %lf \n", a_s);
-            }
-        }
-        a_1[i] = a_s;
-        //printf("a1 = %lf \n", a_1[i]);
-    }
-
-    for(i=0;i<n;i++){
-        for(j = 0; j < n; j++){
-            xp[i] = xp[i] + (a[i][j] * x[j]);
-        }
-    }
-
-    for(i = 0; i < n; i++){
-        do{
-            x[i] = (b[i] - xp[i]) / a[i][i];
-            delta = fabs(xp[i] - x[i]);
-            xp[i] = x[i];
-        }while(delta > e);
-    }
-
-    for(i = 0; i < n; i++){
-        printf("%lf", x[i]);
-    }
-    printf("Hello");*/
-    //delta = 0;
-
-    /*for(i = 0; i < n; i++){
-        if(fabs(x[i] - xp[i]) > delta){
-            delta = fabs(x[i] - xp[i]);
-        }
-        x[i] = xp[i];
-    }*/
-
-    /*if (counter>1000){
-        printf(">>>This system can't be solved with this method!!!\n");
-        stop = 0;
-        }*/
-
-
 
     for ( i = 0; i < n; i++)
         free (a[i]);
