80df061adae241db1929b043b461c18080bb9c68
zagorboda <zagorboda@gmail.com>
2019-11-28 10:09:15 +0300

    Lab7

--- Lab7/main.c
+++ Lab7/main.c
@@ -2,11 +2,22 @@
 #include <stdlib.h>
 #include <math.h>
 
+double func1(float c, float t){
+    return (cos(t / c) - 2*sin(1/c) + 1/c);
+}
+
+double func2(float c, float t){
+    return (sin(log(c) - cos(log(c)) + t*log(c)));
+}
+
 int main()
 {
 
     double t, a1, a2, e, x, f_a, f_x;
+    char f;
 
+    printf("Enter function(1, 2):");
+    scanf("%s", &f);
     printf("Enter t: ");
     scanf("%lf", &t);
     printf("Enter a1: ");
@@ -16,24 +27,32 @@ int main()
     printf("Enter e: ");
     scanf("%lf", &e);
 
-    double calculate(float c){
-        return (cos(t / c) - 2*sin(1/c) + 1/c);
-    }
-
-    while(fabs(a2 - a1) > e){
-        printf("Itterate \n");
-        x = (a1 + a2) / 2;
-        /*f_x = cos(t / x) - 2*sin(1 / x) + 1 / x;
-        f_a = cos(t / a) - 2*sin(1 / a) + 1 / a;*/
-        if(calculate(x) * calculate(a1) > 0){
-            a1 = x;
-            printf("a = %lf \n", a1);
-        }else{
-            a2 = x;
-            printf("b = %lf \n", a2);
-        }
-        printf("x = %lf \n", x);
-        if(calculate(x) == 0)
+    switch(f){
+        case '1':
+            while(fabs(a2 - a1) > e){
+                x = (a1 + a2) / 2;
+                if(func1(x,t) * func1(a1,t) > 0){
+                    a1 = x;
+                    printf("a = %lf \n", a1);
+                }else{
+                    a2 = x;
+                    printf("b = %lf \n", a2);
+                }
+                printf("x = %lf \n", x);
+            }
+            break;
+        case '2':
+            while(fabs(a2 - a1) > e){
+                x = (a1 + a2) / 2;
+                if(func2(x,t) * func2(a1,t) > 0){
+                    a1 = x;
+                    printf("a = %lf \n", a1);
+                }else{
+                    a2 = x;
+                    printf("b = %lf \n", a2);
+                }
+                printf("x = %lf \n", x);
+            }
             break;
     }
 
