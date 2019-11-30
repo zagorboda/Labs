a906962ee465d6a453e25572aacdabcc6ba271b8
zagorboda <zagorboda@gmail.com>
2019-11-30 14:51:40 +0300

    Added functions that check int/float input

--- Lab8/main.c
+++ Lab8/main.c
@@ -10,13 +10,64 @@ struct scheme{
         double C;
 }S;
 
+int int_check(){
+    char num[100];
+    int i,check;
+    while(1){
+        check = 0;
+        scanf("%s", &num);
+            for (i = 0; i < strlen(num); i++){
+                if (isdigit(num[i])){
+                    check += 1;
+                }
+                else
+                    break;
+            }
+
+            if(check == strlen(num) && atoi(num) >= 0){
+                return atoi(num);
+                break;
+            }else{
+                printf("Incorrect input. Try another value \n");
+            }
+    }
+}
+
+double float_check(){
+    char num[100];
+    int i,check,dot;
+
+    while(1){
+        dot = 0;
+        check = 0;
+        scanf("%s", &num);
+        for (i = 0; i < strlen(num); i++){
+            if ((num[i] == 46 || isdigit(num[i])) && dot <=1 ){
+                check += 1;
+                if(num[i] == 46)
+                    dot += 1;
+                printf("%c \n", num[i]);
+            }
+            else
+                break;
+        }
+
+        if(check == strlen(num)){
+            return atof(num);
+            break;
+        }else{
+            printf("Incorrect input. Try another value \n");
+        }
+    }
+}
+
 //struct scheme *p;
 
 void calculate(func, f_min, f_max, d_f){
     printf("f_0 = %lf\n\n", 1/(2*M_PI*sqrt(S.L*S.C)));
     while(f_min <= f_max){
         switch(func){
-            case '1':
+            case 1:
                 printf("f = %d \n\n", f_min);
                 printf("\t%lf - i*%lf \n", S.L/S.C, S.r/(S.C*2*M_PI*f_min));
                 printf("Z (%d) = --------------------\n", f_min);
@@ -24,7 +75,7 @@ void calculate(func, f_min, f_max, d_f){
                 printf("\n\n");
                 break;
 
-            case '2':
+            case 2:
                 printf("f = %d \n\n", f_min);
                 printf("\t%lf + i*%lf \n", S.L/S.C, S.r/(S.C*2*M_PI*f_min));
                 printf("Z (%d) = --------------------\n", f_min);
@@ -32,7 +83,7 @@ void calculate(func, f_min, f_max, d_f){
                 printf("\n\n");
                 break;
 
-            case '3':
+            case 3:
                 printf("f = %d \n\n", f_min);
                 printf("\t%lf + i*%lf \n", S.r1*S.r2, S.r1*(2*M_PI*f_min*S.L - 1/S.C*2*M_PI*f_min));
                 printf("Z (%d) = --------------------\n", f_min);
@@ -40,7 +91,7 @@ void calculate(func, f_min, f_max, d_f){
                 printf("\n\n");
                 break;
 
-            case '4':
+            case 4:
                 printf("f = %d \n\n", f_min);
                 printf("\t%lf + i*%lf \n", S.r1*S.r2 + S.L/S.C, (2*M_PI*f_min*S.L*S.r1 - S.r2/S.C*2*M_PI*f_min));
                 printf("Z (%d) = --------------------\n", f_min);
@@ -53,33 +104,41 @@ void calculate(func, f_min, f_max, d_f){
 }
 
 int main(){
-    char func;
-    int f_min, f_max, d_f;
+    int func,f_min, f_max, d_f;
 
-    printf("Choose a scheme (1,2,3,4):\n");
-    scanf("%c", &func);
-    printf("Enter f_min: ");
-    scanf("%d", &f_min);
-    printf("Enter f_max: ");
-    scanf("%d", &f_max);
-    printf("Enter d_f: ");
-    scanf("%d", &d_f);
-
-    if(func == '1' || func == '2'){
-        printf("Enter R: ");
-        scanf("%lf", &S.r);
+    while(1){
+        printf("Choose scheme(1,2,3,4):\n");
+        func = int_check();
+        if(func == 1 || func == 2 || func == 3 || func == 4)
+            break;
+        else
+            printf("Scheme not found. Try another one.");
     }
-    if(func == '3' || func == '4'){
+
+    printf("Enter f_min: ");
+    f_min = int_check();
+
+    printf("Enter f_max: ");
+    f_max = int_check();
+
+    printf("Enter d_f: ");
+    d_f = int_check();
+
+    if(func == 1 || func == 2){
+        printf("Enter R: ");
+        S.r = float_check();
+    }
+    if(func == 3 || func == 4){
         printf("Enter R1: ");
-        scanf("%lf", &S.r1);
+        S.r1 = float_check();
         printf("Enter R2: ");
-        scanf("%lf", &S.r2);
+        S.r2 = float_check();
     }
 
     printf("Enter L: ");
-    scanf("%lf", &S.L);
+    S.L = float_check();
     printf("Enter C: ");
-    scanf("%lf", &S.C);
+    S.C = float_check();
 
     /*sstruct scheme S = {r1, r2, L, Z};
     S.r1 = r1;
