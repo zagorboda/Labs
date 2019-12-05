f4141875bfd87a4c552235f40caab66787afa800
zagorboda <zagorboda@gmail.com>
2019-12-05 23:43:54 +0300

    Added some UX features.Refactored and debugged code

--- Lab9/main.c
+++ Lab9/main.c
@@ -15,6 +15,7 @@ void write_file();
 void delete_record();
 void delete_file();
 void edit_record();
+void sort_records();
 
 struct record{
     char *name[1000][260];
@@ -127,7 +128,6 @@ void create_file(){
                     de->d_name[strlen(de->d_name) - 4] = '\0';
                     strcpy(list[i], de->d_name);
                     addr[i] = list[i];
-                    //fputs(addr[i], stdout);
                     i++;
                 }
         }
@@ -141,7 +141,7 @@ void create_file(){
                     overwrite = int_check();
                     if(overwrite == 1 || overwrite == 2){
                         if(overwrite == 2){
-                                system("cls");
+                            system("cls");
                             create_file();
                         }
                         break;
@@ -157,13 +157,13 @@ void create_file(){
     if(file_exists == 0 || overwrite == 1){
         if ((fp = fopen(name, "w")) == NULL){
             printf("Some problems occurred. Can't create file");
-            getch();
         }else{
-            printf("File %s successfully created", name);
-            getch();
+            printf("File %s successfully created\n", name);
         }
     }
 
+    printf("To open main menu press any button.\n");
+    getch();
     system("cls");
     main();
 }
@@ -173,7 +173,7 @@ void read_file(){
     int MAXCHAR = 1000;
     char str[253];
     char name[260];
-    char line [ 128 ];
+    char line [128];
     int i = 0;
 
     while(1){
@@ -194,18 +194,24 @@ void read_file(){
         i++;
     }
 
-    printf("\nTo open main menu press any button");
-    getch();
-    system("cls");
-    main();
+    printf("\nTo continue press any button. To open main menu press ESC button.\n");
+    if(getch() == 27){
+        system("cls");
+        main();
+    }else{
+        system("cls");
+        read_file();
+    }
+
 }
 
 void write_file(){
-    char name[260];
     FILE *fp;
+    char name[260];
     int MAXCHAR = 1000;
     char str[MAXCHAR];
 
+    printf("***  All your previous records in file will be deleted  ***\n");
     printf("Enter name of the file to open it. All files in your directory:\n");
 
     struct dirent *de;  // Pointer for directory entry
@@ -225,17 +231,24 @@ void write_file(){
         }
         closedir(dr);
 
-        scanf("%s", &name);
-        strncat(name, ".txt", 4);
-        if ((fp = fopen(name, "w")) == NULL){
-            printf("Can't open file.\n");
-        }else{
+        while(1){
+            printf("Enter file name:\n");
+            scanf("%s", &name);
+            strncat(name, ".txt", 4);
+            if ((fp = fopen(name, "r+")) == NULL){
+                printf("Can't open file.");
+            }else{
+                break;
+            }
+        }
             getchar(); // Після scanf залишається символ нової лінії \n заллишається і тригерить fgets, який одразу закривається.
             // getchar відловлює цей символ .
             int i = 0;
             while(1){
                 printf("Enter region name:\n");
-                fgets(record.name[i], 1000, stdin);
+                do{
+                    fgets(record.name[i], 1000, stdin);
+                }while(strlen(record.name[i]) <= 1);// Використовуєм 1 замість нуля оскільки є нульовий байт \0
                 printf("Enter region square :\n");
                 record.square[i] = float_check();
                 printf("Enter region population:\n");
@@ -251,7 +264,7 @@ void write_file(){
                 getchar();
             }
             fclose(fp);
-        }
+
 
         /*if ((fp = fopen(name, "r")) == NULL){
             printf("Can't open file.");
@@ -259,7 +272,15 @@ void write_file(){
             while (fgets(str, MAXCHAR, fp) != NULL)
             printf("%s", str);
         }*/
+    }
 
+    printf("To write data to another file press any button.To open main menu pres ESC button\n");
+    if(getch() == 27){
+        system("cls");
+        main();
+    }else{
+        system("cls");
+        write_file();
     }
 }
 
@@ -304,9 +325,9 @@ void delete_record(){
         char *addr[100];
 
         while ( fgets ( line, sizeof line, fp ) != NULL ){
-            /*if(i % 3 == 0)
-                printf("%d.\n", i/3 + 1);*/
-            //fputs ( line, stdout );
+            if(i % 3 == 0)
+                printf("\n%d.\n", i/3 + 1);
+            fputs ( line, stdout );
             strcpy(list[i], line);
             i++;
         }
@@ -329,6 +350,7 @@ void delete_record(){
         }
 
         for(i=0;i<number/3 - 1;i ++){
+            printf("\n%d.\n", i + 1);
             printf("%s", record.name[i]);
             printf("%lf\n", record.square[i]);
             printf("%d\n", record.population[i]);
@@ -389,13 +411,18 @@ void delete_record(){
             fprintf(fp, "%d\n", record.population[i]);
         }
 
-        printf("\nRecords are written to file\n");
-        getch();
+        printf("\nRecords are deleted from file\n");
         fclose(fp);
     }
 
-    system("cls");
-    main();
+    printf("To continue press any button. To open main menu press ESC button.\n");
+    if(getch() == 27){
+        system("cls");
+        main();
+    }else{
+        system("cls");
+        delete_record();
+    }
 }
 
 void delete_file(){
@@ -415,7 +442,7 @@ void delete_file(){
         // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html  for readdir()
         int i=0;
         while ((de = readdir(dr)) != NULL){
-                //printf("%s\n", de->d_name); для виводу всіх файлів
+                //printf("%s\n", de->d_name);
                 size_t len = strlen(de->d_name);
                 if(len > 4 && strcmp(de->d_name + len - 4, ".txt") == 0){
                     de->d_name[strlen(de->d_name) - 4] = '\0';
@@ -501,16 +528,19 @@ void edit_record(){
             printf("\n%d.\n", i + 1);
             printf("%s", record.name[i]);
             printf("%lf\n", record.square[i]);
-            printf("%d\n", record.population[i]);
+            printf("%d\n", record  .population[i]);
         }
 
         printf("\nEnter number of record you want to edit:\n");
         int position = int_check();
 
         getchar();
-
+        int length;
+        length = strlen(record.name[position-1]);
         printf("Enter region name:\n");
-        fgets(record.name[position-1], 1000, stdin);
+        do{
+            fgets(record.name[position-1], 1000, stdin);
+        }while(strlen(record.name[position - 1]) <= 1);
         printf("Enter region square :\n");
         record.square[position-1] = float_check();
         printf("Enter region population:\n");
@@ -530,7 +560,6 @@ void edit_record(){
             printf("%d\n", record.population[i]);
         }
 
-        printf("\nNumber == %d\n", number);
         fclose(fp);
 
         if ((fp = fopen(name, "w")) == NULL){
@@ -544,12 +573,87 @@ void edit_record(){
         }
         fclose(fp);
     }
+
+    printf("To continue press any button. To open main menu press ESC button.\n");
+    if(getch() == 27){
+        system("cls");
+        main();
+    }else{
+        system("cls");
+        edit_record();
+    }
+}
+
+void sort_records(){
+    char name[260];
+    FILE *fp;
+    char line[1000];
+    int i=0;
+
+    printf("Enter name of the file to open it. All files in your directory:\n");
+
+    struct dirent *de;  // Pointer for directory entry
+    // opendir() returns a pointer of DIR type.
+    DIR *dr = opendir(".");
+    if (dr == NULL) { // opendir returns NULL if couldn't open directory
+        printf("Could not open current directory" );
+    }else{
+        // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html  for readdir()
+        while ((de = readdir(dr)) != NULL){
+                //printf("%s\n", de->d_name); для виводу всіх файлів
+                size_t len = strlen(de->d_name);
+                if(len > 4 && strcmp(de->d_name + len - 4, ".txt") == 0){
+                    de->d_name[strlen(de->d_name) - 4] = '\0';
+                    printf("%s\n", de->d_name);
+                }
+        }
+        closedir(dr);
+
+        while(1){
+            scanf("%s", &name);
+            strncat(name, ".txt", 4);
+            if ((fp = fopen(name, "r+")) == NULL){
+                printf("Can't open file.Try another name of file:\n");
+            }else{
+                break;
+            }
+        }
+        getchar();
+
+        char list[100][100];
+        char *addr[100];
+
+        while ( fgets ( line, sizeof line, fp ) != NULL ){
+            if(i % 3 == 0)
+                printf("\n%d.\n", i/3 + 1);
+            fputs ( line, stdout );
+            strcpy(list[i], line);
+            i++;
+        }
+
+        int j, number;
+        number = i;
+        j=0;
+        int sort_operator=0, sort_order=0;
+
+        printf("Enter sorting operator(1 - name;2 - square; 3 - population)");
+        while(1){
+            sort_operator = int_check();
+            if(sort_operator == 1 || sort_operator == 2 || sort_operator == 3)
+                break;
+        }
+        printf("Enter order to sort(1 - ''+'',2 - ''-'')");
+        while(1){
+            sort_order = int_check();
+            if(sort_order == 1 || sort_order == 2)
+                break;
+        }
+
+    }
 }
 
 int main(){
-
     //system("chcp 1251"); //Підтримка кирилиці
-
     int action;
 
     printf("Enter action:\n");
@@ -559,8 +663,7 @@ int main(){
     printf("4.Delete records from file\n");
     printf("5.Delete file\n");
     printf("6.Edit record\n");
-    //printf("2.Create test file\n");
-    //printf("3.Create new file and write data\n");
+    printf("7.Sort records\n");
     action = int_check();
     switch(action){
         case 1:
@@ -581,6 +684,9 @@ int main(){
         case 6:
             system("cls");
             edit_record();
+        case 7:
+            system("cls");
+            sort_records();
     }
 
     getch();
