9df15e99d1c15505a7303eaee8deeadc09942a0e
zagorboda <zagorboda@gmail.com>
2019-12-17 12:07:33 +0300

    Added both methods for both equations.Added choosing method

--- Lab7/main.c
+++ Lab7/main.c
@@ -19,11 +19,19 @@ double second_derivative1(double c, double t){
 }
 
 double derivative2(double c, double t){
-    return ((-t*t/c*c*c*c)*cos(t/c) - (2/c*c*c*c)*sin(1/c) + 2/c*c*c);
+    return ((cos(log(c)) + sin(log(c)) + t) / c);
 }
 
-double second_derivative2(double c, double t){
+/*double derivative2(double c, double t){
+    return ((-t*t/c*c*c*c)*cos(t/c) - (2/c*c*c*c)*sin(1/c) + 2/c*c*c);
+}*/
+
+/*double second_derivative2(double c, double t){
     return ( -sin(log(c))/(c*c) + cos(log(c))/(c*c) - t/(c*c) + 1/c );
+}*/
+
+double second_derivative2(double c, double t){
+    return ((-2/(c*c)) * (sin(log(c)) + 1));
 }
 
 /*double second_derivative2(double c, double t){
@@ -81,11 +89,10 @@ double float_check(){
     }
 }
 
-int main()
-{
+int main(){
 
     double t, a1, a2, e, x, f_a, f_x;
-    int f;
+    int f,method;
 
     while(1){
         printf("Enter function(1, 2):");
@@ -94,6 +101,15 @@ int main()
             break;
         else
             printf("Function number is incorrect. Try another one");
+    }
+
+    while(1){
+        printf("Choose method(1 - bisection, 2 - tangent):");
+        method = int_check();
+        if(method == 1 || method == 2)
+            break;
+        else
+            printf("Method number is incorrect. Try another one");
     }
 
     printf("Enter t: ");
@@ -105,6 +121,17 @@ int main()
     printf("Enter a2: ");
     a2 = float_check();
 
+        /*switch(f){
+            case 1:
+                if(func1(a1,t) * func1(a2,t) > 0)
+                    printf("Func 1 must have different values on the edges");
+                break;
+            case 2:
+                if(func1(a2,t) * func2(a2,t) > 0)
+                    printf("Func 2 must have different values on the edges");
+                break;
+        }*/
+
     printf("Enter e: ");
     e = float_check();
 
@@ -112,85 +139,88 @@ int main()
 
     switch(f){
         case 1:
-            /*while(fabs(a2 - a1) > e){
-                x = (a1 + a2) / 2;
-                if(func1(x, t) == 0){
-                    break;
+
+            if(method == 1){
+                printf("\nMethod1\n");
+                while(fabs(a2 - a1) > e){
+                    x = (a1 + a2) / 2;
+                    if(func1(x, t) == 0){
+                        break;
+                    }
+                    if(func1(x,t) * func1(a1,t) > 0){
+                        a1 = x;
+                        printf("a = %lf \n", a1);
+                    }else{
+                        a2 = x;
+                        printf("b = %lf \n", a2);
+                    }
+                    printf("x = %lf \n\n", x);
                 }
-                if(func1(x,t) * func1(a1,t) > 0){
-                    a1 = x;
-                    printf("a = %lf \n", a1);
-                }else{
-                    a2 = x;
-                    printf("b = %lf \n", a2);
-                }
-                printf("x = %lf \n\n", x);
-            }*/
-
-
-
-            if(func1(a1, t) * second_derivative1(a1, t) > 0)
-                x = a1;
-            if(func1(a2, t) * second_derivative1(a2, t) > 0)
-                x = a2;
-            //x = func2(a2, t);
-            //printf("x1 = %lf \n", x);
-            do{
-                xp = x;
-                x = xp - func1(xp, t) / derivative1(xp, t);
+            }else{
+                printf("\nMethod2\n");
+                if(func1(a1, t) * second_derivative1(a1, t) > 0)
+                    x = a1;
+                if(func1(a2, t) * second_derivative1(a2, t) > 0)
+                    x = a2;
+                do{
+                    xp = x;
+                    x = xp - func1(xp, t) / derivative1(xp, t);
+                    printf("x = %lf \n", x);
+                }while(fabs(x - xp) > e);//func2(a2, t) / derivative(a2, t) > e
                 printf("x = %lf \n", x);
-            }while(fabs(x - xp) > e);//func2(a2, t) / derivative(a2, t) > e
-            printf("x = %lf \n", x);
+            }
 
             break;
         case 2:
+            if(method == 1){
+                printf("\nMethod2\n");
+                while(fabs(a2 - a1) > e){
+                    x = (a1 + a2) / 2;
+                    if(func2(x, t) == 0){
+                        break;
+                    }
+                    if(func2(x,t) * func2(a1,t) > 0){
+                        a1 = x;
+                        printf("a = %lf \n", a1);
+                    }else{
+                        a2 = x;
+                        printf("b = %lf \n", a2);
+                    }
+                    printf("x = %lf \n\n", x);
+                }
+            }else{
+            printf("\nMethod2\n");
             if(func2(a1, t) * second_derivative2(a1, t) > 0)
                 x = a1;
             if(func2(a2, t) * second_derivative2(a2, t) > 0)
                 x = a2;
-            //x = func2(a2, t);
-            //printf("x1 = %lf \n", x);
             do{
                 xp = x;
-
                 x = xp - func2(xp, t) / derivative2(xp, t);
                 printf("x = %lf \n", x);
 
-                /*x = a2 - func2(a2, t) / derivative(a2, t);
+            }while(fabs(x - xp) > e);//func2(a2, t) / derivative(a2, t) > e
+            printf("x = %lf \n", x);
+            break;
+
+            /*x = a2 - func2(a2, t) / derivative(a2, t);
+                a2 = x;
+
+                printf("x = %lf \n", x);
+                printf("b = %lf \n", a2);*/
+
+            /*x = a2 - func2(a2, t) / derivative(a2, t);
                 delta = func2(a2, t) - x;
                 a2 = a2 - func2(a2, t) / derivative(a2, t);*/
 
                 /*delta = func(a1+x, t) - func2(x, t);
                 printf("delta = %lf \n", delta);
                 x = x - delta;*/
-
-                /*x = a2 - func2(a2, t) / derivative(a2, t);
-                a2 = x;
-
-                printf("x = %lf \n", x);
-                printf("b = %lf \n", a2);*/
-
-
-
-                /*x = (a1 + a2) / 2;
-                if(func2(x, t) == 0){
-                    break;
-                }
-                if(func2(x,t) * func2(a1,t) > 0){
-                    a1 = x;
-                    printf("a = %lf \n", a1);
-                }else{
-                    a2 = x;
-                    printf("b = %lf \n", a2);
-                }
-                printf("x = %lf \n", x);*/
-            }while(fabs(x - xp) > e);//func2(a2, t) / derivative(a2, t) > e
-            printf("x = %lf \n", x);
-            break;
+            }
     }
-
     printf("%lf \n", x);
 
 
     return 0;
+
 }
